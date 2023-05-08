73d972156fbb25e0ad3f9d90ef8b2d018ea57294
zach <zach@web3technologies.io>
2023-05-07 21:08:21 -0500

    comment line of code

--- deploy/scripts/build_application.sh
+++ deploy/scripts/build_application.sh
@@ -32,7 +32,7 @@ echo "VENV created"
 . "${JENKINS_VENV_DIR}/bin/activate"
 pip install --upgrade pip
 pip install wheel
-pip install -e $jenkins_proj_path .
+# pip install -e $jenkins_proj_path .
 # python setup.py bdist_wheel 
 # deactivate
 # echo "*** Purse Module Created***"
