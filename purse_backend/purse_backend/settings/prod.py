2c3aee026a9050f39cb61ca8adcf7289c542f314
zach <zach@web3technologies.io>
2023-06-11 07:47:14 -0500

    update app url

--- purse_backend/purse_backend/settings/prod.py
+++ purse_backend/purse_backend/settings/prod.py
@@ -7,7 +7,7 @@ DEBUG = False
 ALLOWED_HOSTS = ["*"]
 
 CORS_ORIGIN_WHITELIST = [
-    "https://purse.health"
+    "https://app.purse.health"
 ]
 
 
@@ -28,3 +28,19 @@ PLAID_API_ENVIRONMENT_CONFIGURATION = configuration = Configuration(
                 'plaidVersion': '2020-09-14'
             }
         )
+
+CELERY_BROKER_URL = "sqs://"
+CELERY_TASK_DEFAULT_QUEUE = 'idea_jet_prod'
+
+
+CELERY_BROKER_TRANSPORT_OPTIONS = {
+    'region': 'us-east-2',
+    'is_secure': True,
+    'predefined_queues': {
+        'idea_jet_int': {
+            'url': 'https://sqs.us-east-2.amazonaws.com/490305332793/purse_int',
+        },
+    },
+    'visibility_timeout': 3600,
+    'polling_interval': 5.0,
+}
