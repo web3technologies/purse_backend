c88539464f37af1efa65ad7f886f71893bb80dd3
= <=>
2023-11-17 16:11:35 -0600

    add plaid transaction

--- purse_backend/purse_finance/models/__init__.py
+++ purse_backend/purse_finance/models/__init__.py
@@ -2,4 +2,5 @@ from purse_finance.models.budget import Budget
 from purse_finance.models.item import Item
 from purse_finance.models.networth import NetWorth
 from purse_finance.models.plaid_account import PlaidAccount
+from purse_finance.models.plaid_transaction import PlaidTransaction
 from purse_finance.models.crypto_account import CryptoAccount
