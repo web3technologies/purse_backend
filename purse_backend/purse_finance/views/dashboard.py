892fdb6f4098a1675b073d475c4f05d2b9019d7d
zach <zach@web3technologies.io>
2023-12-12 11:12:58 -0600

    refactor view to break code into single responsiblity methods

--- purse_backend/purse_finance/views/dashboard.py
+++ purse_backend/purse_finance/views/dashboard.py
@@ -20,18 +20,37 @@ from purse_finance.serializers import AggregatedAccountSerializer, LastSixMonthD
 
 class DashboardView(APIView):
 
-    # @method_decorator(cache_user_view())
-    def get(self, request, *args, **kwargs):
 
-        today = timezone.now().today().date()
-        date_6_months_ago = today - relativedelta(months=6)
-        
-        transactions = list(
-            PlaidTransaction.objects.filter(
-                plaid_account__user=self.request.user, 
-                date__gte=date_6_months_ago).order_by("date")
+    def __get_stats(self, request):
+        stats_query = PlaidAccount.objects.filter(user=self.request.user).aggregate(
+            cash=Sum(ExpressionWrapper(Round(Case(
+                When(account_type="DEPOSITORY", then='current_balance'),
+                default=Value(0),
+                output_field=DecimalField()
+            ),2), output_field=DecimalField())),
+            assets=Sum(ExpressionWrapper(Round(Case(
+                When(account_type="INVESTMENT", then="current_balance"),
+                default=Value(0),
+                output_field=DecimalField()
+            ),2), output_field=DecimalField())),
+            debt=Sum(ExpressionWrapper(Round(Case(
+                When(account_type__in=["CREDIT", "LOAN"], then="current_balance"),
+                default=Value(0),
+                output_field=DecimalField()
+            ),2), output_field=DecimalField()))
         )
-        budget_this_month = {}
+        stats_serializer = AggregatedAccountSerializer(data=stats_query)
+        if stats_serializer.is_valid(raise_exception=True):
+            stats_data = stats_serializer.data
+
+        return {
+                "cash": stats_data.get("cash"),
+                "assets": stats_data.get("assets"),
+                "debt": stats_data.get("debt"),
+                "unsaved_transactions": PlaidTransaction.objects.filter(plaid_account__user=self.request.user, is_saved=False).count()
+            }
+
+    def __get_last_six_months_data(self, request, date_6_months_ago):
         last_six_month_qs = PlaidTransaction.objects.filter(plaid_account__user=self.request.user, date__gte=date_6_months_ago).annotate(
             month=MonthName("date")).values("month").annotate(
                 income=Sum(
@@ -64,6 +83,10 @@ class DashboardView(APIView):
                 )
         ).order_by("month")
         last_six_month_data_serializer = LastSixMonthDataSerializer(last_six_month_qs, many=True)
+        return last_six_month_data_serializer.data
+
+    def __get_budget_data(self, transactions, today):
+        budget_this_month = {}
         # initialize the values for checking
         curr_date_name_check = transactions[0].date.strftime("%b")
 
@@ -92,36 +115,24 @@ class DashboardView(APIView):
             else:
                 budget_return_data["series"].append(item)
 
-        stats_query = PlaidAccount.objects.filter(user=self.request.user).aggregate(
-            cash=Sum(ExpressionWrapper(Round(Case(
-                When(account_type="DEPOSITORY", then='current_balance'),
-                default=Value(0),
-                output_field=DecimalField()
-            ),2), output_field=DecimalField())),
-            assets=Sum(ExpressionWrapper(Round(Case(
-                When(account_type="INVESTMENT", then="current_balance"),
-                default=Value(0),
-                output_field=DecimalField()
-            ),2), output_field=DecimalField())),
-            debt=Sum(ExpressionWrapper(Round(Case(
-                When(account_type__in=["CREDIT", "LOAN"], then="current_balance"),
-                default=Value(0),
-                output_field=DecimalField()
-            ),2), output_field=DecimalField()))
+        return budget_return_data
+
+    # @method_decorator(cache_user_view())
+    def get(self, request, *args, **kwargs):
+
+        today = timezone.now().today().date()
+        date_6_months_ago = today - relativedelta(months=6)
+        
+        transactions = list(
+            PlaidTransaction.objects.prefetch_related().filter(
+                plaid_account__user=self.request.user, 
+                date__gte=date_6_months_ago).order_by("date")
         )
-        stats_serializer = AggregatedAccountSerializer(data=stats_query)
-        if stats_serializer.is_valid(raise_exception=True):
-            stats_data = stats_serializer.data
         
         data = { 
-            "stats":{
-                "cash": stats_data.get("cash"),
-                "assets": stats_data.get("assets"),
-                "debt": stats_data.get("debt"),
-                "unsaved_transactions": PlaidTransaction.objects.filter(plaid_account__user=self.request.user, is_saved=False).count()
-            },
-            "monthly_data": last_six_month_data_serializer.data,
-            "budget_this_month": budget_return_data,
+            "stats": self.__get_stats(request),
+            "monthly_data": self.__get_last_six_months_data(request, date_6_months_ago),
+            "budget_this_month": self.__get_budget_data(transactions, today),
             "income_this_month": PlaidTransaction.objects.filter(date__year=today.year, date__month=today.month, is_income=True, plaid_account__user=self.request.user).aggregate(amount=Sum("amount")).get("amount", 0),
             "expense_this_month": PlaidTransaction.objects.filter(date__year=today.year, date__month=today.month, is_income=False, plaid_account__user=self.request.user).aggregate(amount=Sum("amount")).get("amount", 0),
             "recent_transactions": PlaidTransactionSerializer(transactions[0:5], many=True).data,
