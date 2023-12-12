cc158f5174cad51dd9ec4b7a91175984fd17d697
zach <zach@web3technologies.io>
2023-12-12 11:03:53 -0600

    update dashboard container for better retrieval

--- purse_backend/purse_finance/views/dashboard.py
+++ purse_backend/purse_finance/views/dashboard.py
@@ -1,8 +1,8 @@
 import datetime
 from dateutil.relativedelta import relativedelta
 
-from django.db.models import Case, When, Sum, Value, CharField, DecimalField, ExpressionWrapper
-from django.db.models.functions import TruncMonth
+from django.db.models import Case, When, Sum, Value, DecimalField, ExpressionWrapper
+from django.db.models.functions import Abs
 from django.utils.decorators import method_decorator
 from django.utils import timezone
 
@@ -31,10 +31,7 @@ class DashboardView(APIView):
                 plaid_account__user=self.request.user, 
                 date__gte=date_6_months_ago).order_by("date")
         )
-
-        monthly_data = []
         budget_this_month = {}
-        largest_bar_value = 0
         last_six_month_qs = PlaidTransaction.objects.filter(plaid_account__user=self.request.user, date__gte=date_6_months_ago).annotate(
             month=MonthName("date")).values("month").annotate(
                 income=Sum(
@@ -53,10 +50,12 @@ class DashboardView(APIView):
                 expense=Sum(
                     ExpressionWrapper(
                         Round(
-                            Case(
-                                When(is_income=False, then="amount"),
-                                default=Value(0),
-                                output_field=DecimalField()
+                            Abs(
+                                Case(
+                                    When(is_income=False, then="amount"),
+                                    default=Value(0),
+                                    output_field=DecimalField()
+                                )
                             ),
                             2
                         ),
@@ -65,22 +64,14 @@ class DashboardView(APIView):
                 )
         ).order_by("month")
         last_six_month_data_serializer = LastSixMonthDataSerializer(last_six_month_qs, many=True)
-        # if last_six_month_data_serializer.is_valid(raise_exception=True):
-        #     last_six_month_data = last_six_month_data_serializer.data
         # initialize the values for checking
         curr_date_name_check = transactions[0].date.strftime("%b")
-        current_income_amount = 0
-        current_expense_amount = 0
-
 
         for transaction in transactions:
-
             current_amount = transaction.amount
             transaction_date = transaction.date
             main_transaction_category = transaction.category.subcategories.first().label
-
             current_date_name = transaction_date.strftime("%b")
-
             if transaction_date.month == today.month:
                 if current_amount < 0:
                     if main_transaction_category not in budget_this_month:
@@ -88,40 +79,6 @@ class DashboardView(APIView):
                     else:
                         budget_this_month[main_transaction_category] -= current_amount
 
-            # if the current month 
-            if current_date_name == curr_date_name_check:
-                if current_amount < 0:
-                    current_expense_amount -= current_amount
-                else:
-                    current_income_amount += current_amount
-            else:
-                monthly_data.append(
-                    {
-                        "month": curr_date_name_check, 
-                        "income": current_income_amount, 
-                        "expense": current_expense_amount
-                    }
-                )
-
-                # block of code to set the largest bar value amount
-                if current_income_amount < current_expense_amount:
-                    largest_value_bar_check = current_expense_amount
-                else:
-                    largest_value_bar_check = current_income_amount
-                if largest_bar_value < largest_value_bar_check:
-                    largest_bar_value = largest_value_bar_check 
-
-                #reset the monthly data for checking
-                ## todo find out if need to set value as current_amount or 0
-                curr_date_name_check = current_date_name
-                if current_amount < 0 :
-                    current_income_amount = 0
-                    current_expense_amount = current_amount
-                else:
-                    current_income_amount = current_amount
-                    current_expense_amount = 0
-
-        monthly_data.reverse()
 
         budget_return_data = {
             "series": [],
@@ -156,19 +113,18 @@ class DashboardView(APIView):
         if stats_serializer.is_valid(raise_exception=True):
             stats_data = stats_serializer.data
         
-        data = {
+        data = { 
             "stats":{
                 "cash": stats_data.get("cash"),
                 "assets": stats_data.get("assets"),
                 "debt": stats_data.get("debt"),
                 "unsaved_transactions": PlaidTransaction.objects.filter(plaid_account__user=self.request.user, is_saved=False).count()
             },
-            "monthly_data": monthly_data,
+            "monthly_data": last_six_month_data_serializer.data,
             "budget_this_month": budget_return_data,
             "income_this_month": PlaidTransaction.objects.filter(date__year=today.year, date__month=today.month, is_income=True, plaid_account__user=self.request.user).aggregate(amount=Sum("amount")).get("amount", 0),
             "expense_this_month": PlaidTransaction.objects.filter(date__year=today.year, date__month=today.month, is_income=False, plaid_account__user=self.request.user).aggregate(amount=Sum("amount")).get("amount", 0),
             "recent_transactions": PlaidTransactionSerializer(transactions[0:5], many=True).data,
-            "largest_bar_value": largest_bar_value
         }
 
 
